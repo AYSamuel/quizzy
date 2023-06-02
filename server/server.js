f72f90345769875a3061d4ca65d8872bb56e91be
unknown <aysamuel007@gmail.com>
2023-06-02 03:57:13 +0200

    model setup

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3500;
-
+require('dotenv/config');
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.listen(PORT, () => console.log(`Server currently running on port ${PORT}`));
